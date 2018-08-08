18de3666623fd6859319317d0429464846167d04
Michael Elias <michaelelias123@gmail.com>
2018-08-08 10:42:42 -0700

    Some basic app functionality

---
+++ artist_service.py
@@ -0,0 +1,22 @@
+artist_info = {
+  'taylor swift': {
+      'image': 'taylor.png',
+      'spotify_link': '06HL4z0CvFAxyc27GXpf02',
+  },
+  'shawn mendes': {
+      'image': 'Shawn.jpg',
+      'spotify_link': '7n2wHs1TKAczGzO7Dd2rGr'
+  },
+  'abba': {
+      'image': 'abba.png',
+      'spotify_link': '0LcJLqbBmaGUft1e9Mm8HV'
+  },
+  'bruno mars': {
+      'image': 'bruno.jpg',
+      'spotify_link': '0du5cEVh5yTK9QJze8zA0C'
+  },
+  'vance joy': {
+      'image': 'vance.jpg',
+      'spotify_link': '10exVja0key0uqUkk6LJRT'
+  }
+}
